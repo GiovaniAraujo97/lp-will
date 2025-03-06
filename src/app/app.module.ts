8225075cc57b0c43940d11357cc7c272da03d58a
Giovani Araujo <giovanijesus1997@gmail.com>
2025-03-05 23:56:21 -0300

    feat: finish design of all pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -22,7 +23,7 @@ import { ContactComponent } from './contact/contact.component';
     CarsComponent,
     ApartmentsComponent,
     MentorshipComponent,
-    ContactComponent
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
